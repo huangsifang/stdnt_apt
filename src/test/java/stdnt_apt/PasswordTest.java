6d9561070873de31d8e044d9a850a5af8b438681
huangsifang <17816869682@163.com>
2018-02-10 23:25:28 +0800

    HashedCredentialsMatcher加密、验证

---
+++ src/test/java/stdnt_apt/PasswordTest.java
@@ -0,0 +1,61 @@
+package stdnt_apt;
+
+import org.apache.commons.beanutils.BeanUtilsBean;
+import org.apache.commons.beanutils.converters.AbstractConverter;
+import org.apache.shiro.crypto.SecureRandomNumberGenerator;
+import org.apache.shiro.crypto.hash.SimpleHash;
+import org.apache.shiro.realm.jdbc.JdbcRealm;
+import org.testng.annotations.Test;
+
+/**
+ * <p>
+ * User: Zhang Kaitao
+ * <p>
+ * Date: 14-1-27
+ * <p>
+ * Version: 1.0
+ */
+public class PasswordTest extends BaseTest {
+
+	@Test
+	public void testGeneratePassword() {
+		String algorithmName = "md5";
+		String username = "liu";
+		String password = "123";
+		String salt1 = username;
+		String salt2 = new SecureRandomNumberGenerator().nextBytes().toHex();
+		int hashIterations = 2;
+
+		SimpleHash hash = new SimpleHash(algorithmName, password, salt1 + salt2, hashIterations);
+		String encodedPassword = hash.toHex();
+		System.out.println(salt2);
+		System.out.println(encodedPassword);
+	}
+
+	@Test
+	public void testHashedCredentialsMatcherWithJdbcRealm() {
+
+		BeanUtilsBean.getInstance().getConvertUtils().register(new EnumConverter(), JdbcRealm.SaltStyle.class);
+
+		// 浣跨敤testGeneratePassword鐢熸垚鐨勬暎鍒楀瘑鐮�
+		login("classpath:shiro-password.ini", "liu", "123");
+	}
+
+	private class EnumConverter extends AbstractConverter {
+		@Override
+		protected String convertToString(final Object value) throws Throwable {
+			return ((Enum) value).name();
+		}
+
+		@Override
+		protected Object convertToType(final Class type, final Object value) throws Throwable {
+			return Enum.valueOf(type, value.toString());
+		}
+
+		@Override
+		protected Class getDefaultType() {
+			return null;
+		}
+
+	}
+}
--- src/test/java/stdnt_apt/testIsPermitted.java
+++ src/test/java/stdnt_apt/testIsPermitted.java
@@ -1,45 +1,20 @@
 package stdnt_apt;
 
 import org.apache.shiro.SecurityUtils;
-import org.apache.shiro.authc.AuthenticationException;
-import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.authz.UnauthorizedException;
-import org.apache.shiro.config.IniSecurityManagerFactory;
 import org.apache.shiro.subject.Subject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class testIsPermitted {
+public class testIsPermitted extends BaseTest {
 
 	private static Logger logger = LoggerFactory.getLogger("testIsPermitted");
 
-	public void login() {
-		// 1、获取SecurityManager工厂，此处使用Ini配置文件初始化SecurityManager
-		IniSecurityManagerFactory factory = new IniSecurityManagerFactory("classpath:shiro.ini");
-		// 2、得到SecurityManager实例 并绑定给SecurityUtils
-		org.apache.shiro.mgt.SecurityManager securityManager = factory.getInstance();
-		SecurityUtils.setSecurityManager(securityManager);
-		// 3、得到Subject及创建用户名/密码身份验证Token（即用户身份/凭证）
-		Subject subject = SecurityUtils.getSubject();
-		UsernamePasswordToken token = new UsernamePasswordToken("2001001", "123456");
-
-		try {
-			// 4、登录，即身份验证
-			subject.login(token);
-		} catch (AuthenticationException e) {
-			// 5、身份验证失败
-			System.out.println("身份验证失败");
-			logger.error("身份验证失败." + e);
-		}
-
-		Assert.assertEquals(true, subject.isAuthenticated()); // 断言用户已经登录
-	}
-
 	@Test
 	public void testIsPermitted() {
-		login();
+		login("classpath:shiro.ini", "2001001", "123456");
 		Subject subject = SecurityUtils.getSubject();
 		// 判断拥有权限：user:create
 		Assert.assertTrue(subject.isPermitted("user:create"));
@@ -55,7 +30,7 @@ public class testIsPermitted {
 
 	@Test(expectedExceptions = UnauthorizedException.class)
 	public void testCheckPermission() {
-		login();
+		login("classpath:shiro.ini", "2001001", "123456");
 		Subject subject = SecurityUtils.getSubject();
 		// 断言拥有权限：user:create
 		// subject.checkPermission("user:create");
